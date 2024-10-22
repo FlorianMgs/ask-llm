648913d6d3400b7e9b72c91feb68e0f260760a70
FlorianMgs <madgic.shrooms@gmail.com>
2024-10-22 14:25:06 +0700

    bump deps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ask-llm",
-    version="0.1.181",
+    version="0.1.182",
     packages=find_packages(),
     url="https://github.com/FlorianMgs/ask-llm",
     license="MIT",
@@ -10,8 +10,8 @@ setup(
     author_email="madgic.shrooms@gmail.com",
     description="The easiest way to supercharge your apps with LLM!",
     install_requires=[
-        "Jinja2",
-        "pillow",
+        "Jinja2>=3.1.3",
+        "pillow>=10.2.0",
         "langchain>=0.3.2",
         "langchain-community>=0.3.1",
         "langchain-core>=0.3.9",
